5d333eb14ced60572d7bfa9eb6e80d65cbc2cfe9
Sreenu Chenna <45401250+csrinu236@users.noreply.github.com>
2022-02-01 13:42:44 +0530

    Create colorsScript.js

---
+++ colorsScript.js
@@ -0,0 +1,11 @@
+try{
+  const val = document.querySelectorAll(".mdc-text-field__input")[1];
+  let clrs = [...document.querySelectorAll(".hex--label")] ;
+  clrs = clrs.filter((clr, index) => { if(index != 0 && index != 10) return clr ;  }) 
+  const ans = clrs.map((clr, index) => `--primary-${(index+1)*100}: ${clr.textContent} ;`).join("") ;
+console.log(ans) ;
+  
+
+}catch(err){
+  console.log(err);
+}
