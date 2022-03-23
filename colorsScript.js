5bc35fd932eeed933b63ebe6332444062b858360
csrinu236 <45401250+csrinu236@users.noreply.github.com>
2022-03-23 09:36:15 +0530

    Update colorsScript.js

--- colorsScript.js
+++ colorsScript.js
@@ -2,6 +2,7 @@ try{
   const val = document.querySelectorAll(".mdc-text-field__input")[1];
   let clrs = [...document.querySelectorAll(".hex--label")] ;
   clrs = clrs.filter((clr, index) => { if(index != 0 && index != 10) return clr ;  }) 
+  clrs = [...clrs].reverese();
   const ans = clrs.map((clr, index) => `--clr-primary-${(10-index-1)}: ${clr.textContent} ;`).join("") ;
 console.log(ans) ;
   
