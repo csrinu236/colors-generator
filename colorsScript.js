29a9ddcfc56794514d953f65f043166a549ef8df
csrinu236 <45401250+csrinu236@users.noreply.github.com>
2022-03-23 09:37:59 +0530

    Update colorsScript.js

--- colorsScript.js
+++ colorsScript.js
@@ -3,7 +3,7 @@ try{
   let clrs = [...document.querySelectorAll(".hex--label")] ;
   clrs = clrs.filter((clr, index) => { if(index != 0 && index != 10) return clr ;  }) 
   clrs = [...clrs].reverse();
-  const ans = clrs.map((clr, index) => `--clr-primary-${(10-index-1)}: ${clr.textContent} ;`).join("") ;
+  const ans = clrs.map((clr, index) => `--clr-primary-${(index+1)}: ${clr.textContent} ;`).join("") ;
 console.log(ans) ;
   
 
